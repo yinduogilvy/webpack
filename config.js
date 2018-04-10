6cc9bee450f990e2e0643040cc19833e23425119
chinazhaghai <zhanghai@artfangao.com>
2018-04-10 18:04:16 +0800

    remove directory nodelib

--- config.js
+++ config.js
@@ -1,24 +1,5 @@
 const path = require('path');
 //百度统计
-/**
- * 
- * CDN                                                     百度统计
- * bj.yindudigital.cn              北京移动                93690926399c3c2010ef1598305646ff
- * ahyd.yindudigital.cn            安徽移动                197b7932aa54edd074c9b169edea9fc
- * shyd.yindudigital.cn            上海移动                84ce82884295b4197f23a770b1c1c89c
- * zh.yindudigital.cn              中航                    26984424300688c49d02f5eed4031a61
- * jsyd.yindudigital.cn            江苏移动                 05ee5754fe6a161e5576ba19582322df
- * zq.yindudigital.cn              中移动政企               239cdedf188ab632c9b176946ccad7fb
- * dx.yindudigital.cn              电信                    fea81dc154ca9556f519c5dc6c1d771c
- * migu.yindudigital.cn            咪咕                    6cca1164ad57902944130e5193cec5cc
- * brookstone.yindudigital.cn      宏图brookstone          c69e413e8efd2eb964460c9b5ac9b5ac
- * hnyd.yindudigital.cn            湖南移动                 b027ab295592b33c732f8e16cdb7b092
- * migudm.yindudigital.cn          咪咕圈圈                 eac030d4614f12ab54ac69f216ee6528
- * zjyd.yindudigital.cn            浙江移动                 d703449a63eadad220b77508bbd3b2f6
-                                   海澜之家                 d56398baa36600d0b4ab251b16f42dbe
- * 
- */
-
 const statsArr = {
     'bj':'93690926399c3c2010ef1598305646ff', //北京移动
     'ahyd':'197b7932aa54edd074c9b169edea9fc',//安徽移动
