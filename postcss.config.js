3a20497a95b38c5059d69e2b0fb6aefed03e3f05
chinazhaghai <zhanghai@artfangao.com>
2018-04-14 11:55:49 +0800

    update

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "precss":{},
     "postcss-cssnext":{},
     "cssnano":{
+      reduceIdents: false,
       preset:"advanced",
       autoprefixer:false,
       "postcss-zindex":false
