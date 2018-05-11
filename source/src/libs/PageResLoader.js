721028e94084095b5566fd5386105175eb055296
chinazhaghai <zhanghai@artfangao.com>
2018-05-11 10:35:47 +0800

    beta

--- source/src/libs/PageResLoader.js
+++ source/src/libs/PageResLoader.js
@@ -14,30 +14,61 @@
      }
 
      init(){
-        let res = $("[data-src]"),self = this;
-        res.each(function(){
-            self.addImage($(this).data("src"),(url)=>{
-                $(this).data("src",url);
-            });
-        });
-        this.on("res:loadProgress",(e)=>{
-            this.trigger("pageRes:loadProgress",...e);
-        });
-        this.on("res:loadComplete",(e)=>{
-            res.each(function(){
-                var url = $(this).data("src"),tagName = $(this).get(0).tagName.toLowerCase();
-                if(tagName=="img"){
-                    $(this).attr('src',url);
-                }else{
-                    $(this).css({"background-image":"url("+url+")"});
-                }
-            });
-            this.trigger("pageRes:loadComplete",...e);
-        });
-        this.start();
+        // let res = $("[data-src]"),self = this;
+        // res.each(function(){
+        //     self.addImage($(this).data("src"),(url)=>{
+        //         $(this).data("src",url);
+        //     });
+        // });
+        // this.on("res:loadProgress",(e)=>{
+        //     this.trigger("pageRes:loadProgress",...e);
+        // });
+        // this.on("res:loadComplete",(e)=>{
+        //     res.each(function(){
+        //         var url = $(this).data("src"),tagName = $(this).get(0).tagName.toLowerCase();
+        //         if(tagName=="img"){
+        //             $(this).attr('src',url);
+        //         }else{
+        //             $(this).css({"background-image":"url("+url+")"});
+        //         }
+        //     });
+        //     this.trigger("pageRes:loadComplete",...e);
+        // });
+        // this.start();
+        var loader = new WxMoment.Loader();
+        this.res = $("[data-src]");
+
+        if(!this.res.length) return this.loadComplete(),false;
+
+        var self = this;
+        this.res.each(function(){
+            var url = self.cdn+$(this).data("src")+(self.version?"?v="+(self.version):"");
+            $(this).data("src",url);
+            loader.addImage(url);
+        })
+
+
+        loader.addProgressListener(this.loadProgress.bind(this));
+        loader.addCompletionListener(this.loadComplete.bind(this));
+
+        loader.start();
         
         
         
+     }
+     loadProgress(e){
+        this.trigger("pageRes:loadProgress", {loaded:e.completedCount,total:e.totalCount});
+     }
+     loadComplete(){
+        this.res.each(function(){
+            var url = $(this).data("src"),tagName = $(this).get(0).tagName.toLowerCase();
+            if(tagName=="img"){
+            $(this).attr('src',url);
+            }else{
+            $(this).css({"background-image":"url("+url+")"});
+            }
+        });
+        this.trigger("pageRes:loadComplete");
      }
      
      
